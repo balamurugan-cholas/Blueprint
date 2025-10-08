f562166a332f8d13598fb691bb963994fab271b5
Balamurugan <balamuruganofficial3@gmail.com>
2025-10-08 09:02:31 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
     path('generate/', views.generate, name='generate'),
-    path('generate-project/', views.generate_project, name='generate_project'),  # <- add this
+    path('generate-project/', views.generate_project, name='generate_project'),
     path('delete-project/', views.delete_project, name='delete_project'),
     path('open-project/<int:project_id>/', views.open_project, name='open_project'),
     path("search/", views.search_projects, name="search_projects"),
